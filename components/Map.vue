821907033e1459fa2bbd9b1705714c9e4f6dfc98
Craig Stephenson <crstephenson@alaska.edu>
2024-11-05 17:06:44 -0900

    Working Leaflet map with stream segment WMS tiles.

--- components/Map.vue
+++ components/Map.vue
@@ -1,8 +1,36 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const { $L } = useNuxtApp()
+
+onMounted(() => {
+  var map = $L.map('map').setView([37.8, -96], 4)
+
+  $L.tileLayer(
+    'https://basemap.nationalmap.gov/arcgis/rest/services/USGSTopo/MapServer/tile/{z}/{y}/{x}',
+    {
+      maxZoom: 19,
+      attribution: 'Map data © USGS',
+    }
+  ).addTo(map)
+
+  $L.tileLayer
+    .wms('https://gs.earthmaps.io/geoserver/wms', {
+      transparent: true,
+      format: 'image/png',
+      layers: 'hydrology:seg',
+      zIndex: 10,
+    })
+    .addTo(map)
+
+  // Print the current Leaflet zoom level out to the console
+  map.on('zoomend', function () {
+    console.log('Current Zoom: ' + map.getZoom())
+  })
+})
+</script>
 
 <template>
   <div>
-    Component: Map
+    <div id="map" style="height: 500px"></div>
   </div>
 </template>
 
